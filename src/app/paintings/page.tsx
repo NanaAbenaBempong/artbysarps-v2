9efb08eae696b77b45e2d8cac6f7fa2f84dc366c
NanaAbenaBempong <bempong.n@northeastern.edu>
2026-04-01 03:56:00 -0400

    Update src/app/paintings/page.tsx

--- src/app/paintings/page.tsx
+++ src/app/paintings/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Nav from '../components/Nav'
-import UnrestrainedGallery from '../components/UnrestrainedGallery'
+import GeometricGallery from '../components/GeometricGallery'
 
 const natureCollection = [
   {
